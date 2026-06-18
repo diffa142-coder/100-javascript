416fd21adab5f8afa0e0684c4154a210221051da
diffa <diffa142@gmail.com>
2026-06-18 08:16:57 +0700

    penyelesaian

---
+++ Looping/19.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+let arr = [];
+
+while (true) {
+    let pilihan = prompt('masukan pilihan angka atau ketik (selesai): ');
+    if (pilihan === 'selesai') {
+        break;
+    } 
+
+    let anggka = parseInt(pilihan);
+    if (!isNaN(pilihan)) {
+        arr.push(anggka)
+    } else {
+        console.log('input salah');
+    }
+}
+
+if (arr.length > 0) {
+    let jumlah = 0
+
+    for (let nilai of arr){
+        jumlah += nilai;
+    }
+
+    let rataRata = jumlah / arr.length;
+    console.log(arr)
+    console.log(`Rata-rata nilai    : ${rataRata}`);
+}
